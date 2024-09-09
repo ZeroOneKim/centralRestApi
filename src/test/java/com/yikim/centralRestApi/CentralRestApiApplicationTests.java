c80c55c7142bd1cd5e878224c06221cdc2aa2f7c
ZeroOneKim <wowwow0124@naver.com>
2024-09-09 20:58:08 +0900

    초기 Commit.

---
+++ src/test/java/com/yikim/centralRestApi/CentralRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yikim.centralRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
