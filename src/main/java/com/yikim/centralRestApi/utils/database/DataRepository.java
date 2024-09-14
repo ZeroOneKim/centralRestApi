703254b76dd9418e1764388417ffb1f0a6946462
ZeroOneKim <wowwow0124@naver.com>
2024-09-14 22:45:12 +0900

    test Repo

--- src/main/java/com/yikim/centralRestApi/utils/database/DataRepository.java
+++ src/main/java/com/yikim/centralRestApi/utils/database/DataRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DataRepository extends JpaRepository<DataEntity, Long> {
-
+public interface DataRepository extends JpaRepository<DataEntity, String> {
+    DataEntity findByName(String name);
 }
