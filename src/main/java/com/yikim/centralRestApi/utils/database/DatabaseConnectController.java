552e7f666f9110e911b7bd5b4e1af3909f002a0e
ZeroOneKim <wowwow0124@naver.com>
2024-09-14 22:52:40 +0900

    change Controller

---
+++ src/main/java/com/yikim/centralRestApi/utils/database/DatabaseConnectController.java
@@ -0,0 +1,26 @@
+package com.yikim.centralRestApi.utils.database;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+/**
+ * DB 연결 테스트 목적 컨트롤러
+ */
+@RestController
+public class DatabaseConnectController {
+    @Autowired
+    private DataRepository dataRepository;
+
+    @GetMapping("/data/connect")
+    public String connectDBInfo() {
+        Optional<DataEntity> dataEntity = dataRepository.findById("yikim");
+        System.out.println(dataEntity);
+
+
+        return "Testing...";
+    }
+
+}
