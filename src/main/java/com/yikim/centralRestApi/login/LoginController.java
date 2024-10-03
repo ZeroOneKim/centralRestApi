6b20751c5c989eb751839da26d9aa2813b0c21cf
ZeroOneKim <wowwow0124@naver.com>
2024-10-03 18:32:48 +0900

    API Test

--- src/main/java/com/yikim/centralRestApi/login/LoginController.java
+++ src/main/java/com/yikim/centralRestApi/login/LoginController.java
@@ -1,20 +1,32 @@
 package com.yikim.centralRestApi.login;
 
+import com.yikim.centralRestApi.utils.security.JwtTokenUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
+    @Autowired UserRepository userRepository;
+    @Autowired PasswordEncoder passwordEncoder;
+    @Autowired JwtTokenUtils jwtTokenUtils;
 
     @PostMapping("/api/auth/login-process")
     public ResponseEntity<?> login(@RequestBody UserEntity userEntity) {
         try{
+            UserEntity userObject = userRepository.findByUserId(userEntity.getUserId());
 
-        } catch () {
+            System.out.println("1 : " + userEntity.getUserId());
+            System.out.println("2 : " + userObject.getUserId());
+
+
+
+        } catch (Exception e) {
 
         }
+        return null;
     }
 }
