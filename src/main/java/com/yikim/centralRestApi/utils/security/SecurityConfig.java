8e20464b46161a106b969545fe147645c2b1122c
ZeroOneKim <wowwow0124@naver.com>
2024-10-07 19:55:46 +0900

    1) Cors 설정 2) Encoding

--- src/main/java/com/yikim/centralRestApi/utils/security/SecurityConfig.java
+++ src/main/java/com/yikim/centralRestApi/utils/security/SecurityConfig.java
@@ -4,10 +4,16 @@ import org.apache.catalina.filters.CorsFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 /**
  * Spring Security 의 설정 클래스
@@ -23,15 +29,14 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    @Autowired
-    private CorsFilter corsFilter;
 
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf((value) -> value.disable())
+                .cors(Customizer.withDefaults())
                 .authorizeRequests(req -> req
-                            .requestMatchers("/login").permitAll()
+                            .requestMatchers("/login", "/api/auth/login-process").permitAll()
                             .anyRequest().authenticated()
                             .and())
                 .sessionManagement((session) ->
@@ -40,7 +45,21 @@ public class SecurityConfig {
         return httpSecurity.build();
     }
 
-    //TODO > User DB설계
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*"); // 필요한 도메인으로 변경 가능
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
 
-    //TODO Password Encoding
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
