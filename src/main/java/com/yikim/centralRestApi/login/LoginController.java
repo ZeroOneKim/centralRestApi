d0deeca0269fad887cd2988af08cc3c5b81bd0d2
ZeroOneKim <wowwow0124@naver.com>
2024-10-15 21:54:58 +0900

    signup

--- src/main/java/com/yikim/centralRestApi/login/LoginController.java
+++ src/main/java/com/yikim/centralRestApi/login/LoginController.java
@@ -1,5 +1,6 @@
 package com.yikim.centralRestApi.login;
 
+import com.yikim.centralRestApi.utils.security.SecurityInfo;
 import com.yikim.centralRestApi.utils.security.jwt.JwtTokenUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 /**
@@ -23,6 +25,9 @@ public class LoginController {
     @Autowired UserRepository userRepository;
     @Autowired PasswordEncoder passwordEncoder;
     @Autowired JwtTokenUtils jwtTokenUtils;
+    @Autowired LoginUtilService loginUtilService;
+    @Autowired
+    SecurityInfo securityInfo;
 
     /**
      * login 인증 프로세스
@@ -53,5 +58,31 @@ public class LoginController {
         }
     }
 
-
+    /**
+     * 회원가입 처리 메서드(운영자만 사용)
+     *
+     * @param userEntity : 사용자 정보객체
+     * @param securityForSignUpPassCode : api 추가 패스워드
+     * @return HTTP body
+     */
+    @PostMapping("/api/auth/secret/signup")
+    public ResponseEntity<?> signUp(@RequestBody UserEntity userEntity, String securityForSignUpPassCode) {
+        if(securityForSignUpPassCode != securityInfo.getSignUpSecureKeyVal()) {
+            //TODO : GET IP INFO
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("부적합한 접근.");
+        }
+        if(userRepository.findByUserId(userEntity.getUserId()) != null
+                || userRepository.findByUserId(userEntity.getUserId()).equals(userEntity.getUserId())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("InValid UserId");
+        }
+        //나만 쓸껀데 검증이 필요할까.?
+        try {
+            userEntity.setPassword(passwordEncoder.encode(userEntity.getPassword()));
+            userEntity.setCreateAt(LocalDateTime.now());
+            userRepository.save(userEntity);
+            return ResponseEntity.ok().body("OK.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("InValid Value");
+        }
+    }
 }
