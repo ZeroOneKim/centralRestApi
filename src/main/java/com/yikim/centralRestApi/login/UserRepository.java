d9e66ee839d59533b9cb87ebf4dc32d519e8013a
ZeroOneKim <wowwow0124@naver.com>
2024-10-21 21:35:25 +0900

    null 처리

--- src/main/java/com/yikim/centralRestApi/login/UserRepository.java
+++ src/main/java/com/yikim/centralRestApi/login/UserRepository.java
@@ -3,7 +3,9 @@ package com.yikim.centralRestApi.login;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, String> {
-    UserEntity findByUserId(String userId);
+    Optional<UserEntity> findByUserId(String userId);
 }
