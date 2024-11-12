6a3712b3723bcbccdfc265b0cc544711d70f0031
ZeroOneKim <wowwow0124@naver.com>
2024-11-12 23:11:25 +0900

    WebFlux Test.

--- src/main/java/com/yikim/centralRestApi/utils/database/test/DataRepository.java
+++ src/main/java/com/yikim/centralRestApi/utils/database/test/DataRepository.java
@@ -1,12 +1,16 @@
 package com.yikim.centralRestApi.utils.database.test;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 
 /**
  * Database Connect 테스트.
  */
 @Repository
-public interface DataRepository extends JpaRepository<DataEntity, String> {
-    DataEntity findByName(String name);
+public interface DataRepository extends ReactiveCrudRepository<DataEntity, String> {
+    @Query("SELECT * FROM TEST_CONNECT_TAB WHERE NAME_ = :name")
+    Flux<DataEntity> findByName(@Param("name") String name);
 }
