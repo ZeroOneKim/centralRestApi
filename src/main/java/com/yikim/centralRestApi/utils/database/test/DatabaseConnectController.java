d9046ae8c64714a52108323fc8711fc032e9fbe7
ZeroOneKim <wowwow0124@naver.com>
2024-11-12 23:11:20 +0900

    WebFlux Test.

--- src/main/java/com/yikim/centralRestApi/utils/database/test/DatabaseConnectController.java
+++ src/main/java/com/yikim/centralRestApi/utils/database/test/DatabaseConnectController.java
@@ -1,8 +1,12 @@
 package com.yikim.centralRestApi.utils.database.test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.Optional;
 
@@ -15,12 +19,13 @@ public class DatabaseConnectController {
     private DataRepository dataRepository;
 
     @GetMapping("/data/connect")
-    public String connectDBInfo() {
-        Optional<DataEntity> dataEntity = dataRepository.findById("yikim");
-        System.out.println(dataEntity);
+    public Flux<DataEntity> connectDBInfo() {
 
+        Flux<DataEntity> dataEntity = dataRepository.findByName("yikim");
 
-        return "Testing...";
+        return dataEntity
+                .doOnNext(data -> System.out.println("DataEntity found: " + data))
+                .doOnError(error -> System.out.println("Error occurred: " + error.getMessage()));
     }
 
 }
