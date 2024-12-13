538982d0c6d27a2248c0c3e939825a4675a86253
ZeroOneKim <wowwow0124@naver.com>
2024-12-13 22:33:23 +0900

    필터처리 : 헤더 Authorization => 쿠키방식

--- src/main/java/com/yikim/centralRestApi/utils/security/jwt/JwtAuthFilter.java
+++ src/main/java/com/yikim/centralRestApi/utils/security/jwt/JwtAuthFilter.java
@@ -1,9 +1,10 @@
 package com.yikim.centralRestApi.utils.security.jwt;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import org.springframework.http.HttpCookie;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
@@ -31,8 +32,8 @@ public class JwtAuthFilter implements WebFilter {
 
     /**
      * JWT 토큰 추출, 인증처리 메서드
-     * HTTP 요청의 헤더에 jwt 토큰을 추출하여, 유효성 검증 후 컨텍스트에 설정
-     * Authorization : Bearer [jwt token]
+     * HTTPS 요청의 헤더에 jwt 토큰을 추출하여, 유효성 검증 후 컨텍스트에 설정
+     * Authorization : Bearer [jwt token] -> 쿠키처리로 변경되었음. Cookie : yi-jwt
      *
      * @param exchange : HTTP 요청 및 응답 정보를 포함 객체
      *                   -> ServerWebExchange 는 WebFlux 에서 서버 요청과 응답을 추상화한 객체임. (Req + Res) -> 논블로킹 목적
@@ -41,24 +42,33 @@ public class JwtAuthFilter implements WebFilter {
      */
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        String header = exchange.getRequest().getHeaders().getFirst("Authorization");
+        HttpCookie jwtCookieYI = exchange.getRequest().getCookies().getFirst("yi-jwt");
+        String jwtToken = (jwtCookieYI != null) ? jwtCookieYI.getValue() : null;
 
-        if(header != null && header.startsWith("Bearer ")) {
-            String jwtToken = header.substring(7);
-            try {
-                String username = jwtTokenUtils.extractUser(jwtToken);
+        //jwt 유효성 검사
+        if (jwtToken == null || !jwtTokenUtils.isValidJWT(jwtToken)) return chain.filter(exchange);
 
-                if (username != null) {
-                    UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(username, null, new ArrayList<>()); //TODO Auth 관리
+        try {
+            String username = jwtTokenUtils.extractUser(jwtToken);
 
-                    return chain.filter(exchange)
-                            .contextWrite(ReactiveSecurityContextHolder.withAuthentication(auth)); // Context 설정
-                }
-            } catch (Exception e) {
-                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-                return exchange.getResponse().setComplete();
+            if (username != null) {
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(username, null, new ArrayList<>()); //TODO Auth 관리
+
+                return chain.filter(exchange)
+                        .contextWrite(ReactiveSecurityContextHolder.withAuthentication(auth)); // Context 설정
             }
+        } catch (ExpiredJwtException e) {
+            //만료요..
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            exchange.getResponse().getHeaders().add("Will Refresh Token", "만료됨");
+            return exchange.getResponse().setComplete();
+        } catch (Exception e) {
+            // JWT 토큰 검증 중 오류 처리
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            exchange.getResponse().getHeaders().add("Filter_ERR_Message", "Invalid Token");
+            return exchange.getResponse().setComplete();
         }
+
         return chain.filter(exchange);
     }
 }
