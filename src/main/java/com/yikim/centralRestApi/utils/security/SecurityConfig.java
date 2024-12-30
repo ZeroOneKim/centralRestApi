8b779ce78f45318026ed34845e49d2e02c2f6513
ZeroOneKim <wowwow0124@naver.com>
2024-12-31 03:46:56 +0900

    cors 주소 테스트.

--- src/main/java/com/yikim/centralRestApi/utils/security/SecurityConfig.java
+++ src/main/java/com/yikim/centralRestApi/utils/security/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("https://localhost:3000");
-        config.addAllowedOrigin("https://192.168.0.4");
+        config.addAllowedOrigin("https://192.168.0.4:3000");
         //config.addAllowedOrigin("https://www.yi97-cloud.com");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
