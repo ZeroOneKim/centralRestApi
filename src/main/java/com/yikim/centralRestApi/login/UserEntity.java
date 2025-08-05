d6263d6ed605878f5ac65ac3a46b67afc35b300b
ZeroOneKim <wowwow0124@naver.com>
2025-08-05 20:42:28 +0900

    Annotation refix

--- src/main/java/com/yikim/centralRestApi/login/UserEntity.java
+++ src/main/java/com/yikim/centralRestApi/login/UserEntity.java
@@ -3,19 +3,27 @@ package com.yikim.centralRestApi.login;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
 
+@Table("SY_USER_MT")
 public class UserEntity {
-    @Id
     @Column("USER_ID")
     private String userId;
+    @Column("EMAIL")
     private String email;
+    @Column("USER_NM")
+    private String userNm;
+    @Column("ROLE")
     private String role;
+    @Column("PASSWORD")
     private String password;
-    @Column("rgst_dt")
+    @Column("RGST_DT")
     private LocalDateTime createdAt;
+    @Column("ACCOUNT_STATUS")
     private String accountStatus;
+    @Column("FAILED_CNT")
     private int failedCnt;
 
 
@@ -32,6 +40,12 @@ public class UserEntity {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public String getUserNm() { return userNm;}
+    public void setUserNm(String userNm) { this.userNm = userNm; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) {this.createdAt = createdAt;}
 
     public String getRole() {
         return role;
